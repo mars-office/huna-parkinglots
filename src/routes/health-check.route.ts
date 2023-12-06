b893dc10da930ae4a0ae101c117e9e2845f6da4c
Alin Florin <alin@qoffice.ro>
2023-12-06 23:23:36 +0200

    test

--- src/routes/health-check.route.ts
+++ src/routes/health-check.route.ts
@@ -1,9 +1,10 @@
 import { Request, Response, Router } from "express";
+import { VERSION } from "../version";
 
 const healthCheckRouter = Router();
 
 healthCheckRouter.get("/api/parkinglots/health", (_: Request, res: Response) => {
-  res.send("OK");
+  res.send("OK: " + VERSION);
 });
 
 export default healthCheckRouter;
